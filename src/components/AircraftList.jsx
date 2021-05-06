99beb60c2a80772dc3073ca06deb42281087e2f1
Jonathan Wilhite <jonnygwilhite@gmail.com>
2021-05-06 10:20:18 -0500

    initial setup for Aircrafts section

--- src/components/AircraftList.jsx
+++ src/components/AircraftList.jsx
@@ -1,18 +1,46 @@
-import React, { useEffect } from "react";
+import React, {
+    useState,
+    useEffect,
+    useRef,
+} from "react";
 
 import axios from "axios";
 
 export const AircraftList = () => {
+    const [aircraftList, setAircraftList] = useState([]);
+    const [renderedAircrafts, setRenderedAircrafts] = useState();
+    
+    const aircraftListRef = useRef();
+    const renderedAircraftsRef = useRef();
+    aircraftListRef.current = aircraftList;
+    renderedAircraftsRef.current = renderedAircrafts;
+    
     useEffect(() => {
         axios.get("https://infinite-dawn-93085.herokuapp.com/aircrafts")
             .then(res => {
-                console.log(res.data);
+                setAircraftList(res.data.data);
+                renderAircrafts(aircraftListRef.current)
             });
-    })
+    }, []);
+    
+    const renderAircrafts = (data) => {
+        const renderedValues = data.map((d) => {
+            return (
+                <div className="aircraft-padding" key={d.ident}>
+                    <div className="aircraft-entry">
+                        <div>{d.ident}</div>
+                        <div>0%</div>
+                    </div>
+                </div>
+            );
+        });
+        setRenderedAircrafts(renderedValues);
+    };
     
     return(
-        <div>
+        <div className="something">
             <p>Aircrafts</p>
+            <div className="aircraft-box">{renderedAircraftsRef.current}</div>
         </div>
     )
 };
